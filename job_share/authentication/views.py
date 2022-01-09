2ab6e990eefc6b50f1956511205f4245d2df823a
ranoa <ranoaritsiky@gmail.com>
2022-01-09 22:41:11 +0300

    update signup

--- job_share/authentication/views.py
+++ job_share/authentication/views.py
@@ -3,6 +3,7 @@ from .forms import NewUserForm
 from django.contrib.auth import login
 from django.contrib import messages
 
+
 # Create your views here.
 def index(request):
     return render(request,'authentication/index.html')
@@ -14,7 +15,7 @@ def contact(request):
 def signup(request):
     if request.method == 'POST':
         form=NewUserForm(request.POST)
-        if form.is_user:
+        if form.is_valid():
             user=form.save()
             login(request,user)
             messages.success(request,"Registration done")
